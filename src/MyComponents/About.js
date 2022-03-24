1f2dc0da41373913d78ff732487da339ce5b1c74
Bikash Jena <b2j1999jan27@gmail.com>
2022-03-24 10:59:21 +0530

    Initial commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div class="card">
+            <div class="card-body">
+                Hi, I am Bikash.
+            </div>
+            <div class="card-body">
+                React is cool, isn't it?
+            </div>
+            <div class="card-body">
+                <p class="card-text">Want to know more about me?</p>
+                <a href="https://www.linkedin.com/in/bikash-jena-9ab81215a/" class="btn btn-primary">My LinkedIn</a>
+                <p></p>
+                <a href="https://github.com/b2jena" class="btn btn-primary">My Github</a>
+            </div>
+        </div>
+    )
+}
